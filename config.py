5d1268d16948e30a1a58edd220ce3025e760582d
David Assigbi <assigbid@gmail.com>
2021-12-22 03:22:16 +0100

    Changed providers gateway ip address for simpler readability

--- config.py
+++ config.py
@@ -2,21 +2,21 @@
 import collections
 from models import *
 
-CHECK_INTERVAL = 1
+CHECK_INTERVAL = 0.3
 START_METRIC = 100
 MAX_STORED_TEST_RESULT_COUNT = 50
 DEFAULT_SHELL_OPTIONS = {"shell": True,
                          "text": True, "close_fds": True, "timeout": 5}
 
-MAX_FAILED_CHECKS = 10
+MAX_FAILED_CHECKS = 50
 MIN_CONSECUTIVE_SUCCEEDED_CHECKS = 1
 STICKY_PROVIDER_FILENAME = "sticky.provider"
 
 providers: list[Provider] = [
-    Provider(interface_name="eth1", ipv4_address="10.10.10.1",
-             network="10.10.10.0/24", gateway="10.10.10.254", rt_table_name="R1", rt_table_id=1),
-    Provider(interface_name="eth2", ipv4_address="10.10.10.2",
-             network="10.10.10.0/24", gateway="10.10.10.254", rt_table_name="R2", rt_table_id=2)
+    Provider(id="isp1", interface_name="eth1", ipv4_address="10.10.1.1",
+             network="10.10.1.0/24", gateway="10.10.1.254", rt_table_name="R1", rt_table_id=1),
+    Provider(id="isp2", interface_name="eth2", ipv4_address="10.10.2.2",
+             network="10.10.2.0/24", gateway="10.10.2.254", rt_table_name="R2", rt_table_id=2)
 ]
 
 test_cases: list[TestScenario] = [
